799a8db4a96f6a91bac36a7932151d6008b3c3e9
k-yone <kenji.3.yoneyama@gmail.com>
2018-08-13 02:15:29 +0900

    エディタ表示

--- cookpet-web/src/App.js
+++ cookpet-web/src/App.js
@@ -1,19 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState } from 'draft-js'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
+import firebase from './firebase';
 
-class App extends Component {
+var storageRef = firebase.storage().ref();
+
+class ControlledEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploadedImages: [],
+      editorState: EditorState.createEmpty()
+    };
+  }
+
+  uploadImageCallBack(file) {
+    let uploadedImages = this.state.uploadedImages;
+
+    const imageObject = {
+      file: file,
+      localSrc: URL.createObjectURL(file)
+    }
+
+    uploadedImages.push(imageObject);
+
+    this.setState({uploadedImages: uploadedImages});
+
+    return new Promise((resolve, reject) => {
+      resolve({data: {link: imageObject.localSrc}});
+    });
+  }
+
+
+  onEditorStateChange: Function = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <Editor
+        editorState={editorState}
+        wrapperClassName="demo-wrapper"
+        editorClassName="demo-editor"
+        onEditorStateChange={this.onEditorStateChange}
+        toolbar={{
+          image: {
+            urlEnabled: false,
+            uploadEnabled: true,
+            uploadCallback: (file) => this.uploadImageCallBack(file),
+            previewImage: true,
+            inputAccept: 'image/jpeg,image/jpg,image/png',
+          },
+        }}
+      />
+    )
+  }
+}
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ControlledEditor />
     );
   }
 }
---
+++ cookpet-web/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+const config = {
+  apiKey: "AIzaSyAn9R6tTLhpazNyP5_HVZxrskkmRivSYkI",
+  authDomain: "cookpet-7b0b3.firebaseapp.com",
+  databaseURL: "https://cookpet-7b0b3.firebaseio.com",
+  projectId: "cookpet-7b0b3",
+  storageBucket: "cookpet-7b0b3.appspot.com",
+  messagingSenderId: "212142476354"
+};
+firebase.initializeApp(config);
+
+export default firebase;
